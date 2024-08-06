589b3c77dc345b6e60d55a867eb7a486899a1c09
Sanjay Sanjay <sanjaysanjay@sanjay15.local>
2024-08-06 22:23:37 +0530

    completed main pg

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-black p-2 flex justify-around'>
+        <h1>Bot2Do</h1>
+        <ul>
+            <li>Home</li>
+            <li>Service</li>
+            <li>About</li>
+        </ul>
+    </header>
+  )
+}
+
+export default Header
--- src/Main.jsx
+++ src/Main.jsx
@@ -4,6 +4,7 @@ import { RiSendPlaneFill } from "react-icons/ri";
 const Main = () => {
   return (
     <main className='w-full h-screen p-16 flex flex-col items-center  bg-slate-950 text-white flex flex-col justify-between'>
+
         <section className='p-6 px-11 overflow-auto'>
             <section className='flex flex-col leading-loose'>
                 <h3 className='font-bold flex wrap items-center gap-2'><img className='w-6' src="https://imgs.search.brave.com/FlXynuyZEqoioqvkFHJUTERg914x6sm9BB5K64Q7nbE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9hc3Nl/dHMuc3RpY2twbmcu/Y29tL2ltYWdlcy81/ODBiNTdmYmQ5OTk2/ZTI0YmM0M2MwNTEu/cG5n" alt="" />Agent M <small className='text-gray-600 text-xs'> Just now</small></h3>
@@ -19,8 +20,7 @@ const Main = () => {
                 </div>
             </section>
         </section>
-
-
+        
         <section className='sticky fixed top-12 w-2/4 '>
             <input className='w-full bg-slate-950 border border-gray-400 rounded p-2 px-4 outline-none' type="text" placeholder='Your message here' id="" />
             <RiSendPlaneFill className='absolute top-2 text-gray-400 text-2xl right-6' tabIndex={1} />
