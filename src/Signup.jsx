2be8d9f9f2fb826ee00f235c30334b00a72df1c8
Sanjay Sanjay <sanjaysanjay@sanjay15.local>
2024-08-06 19:11:15 +0530

    defined basic syntax

---
+++ src/Signup.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <main>
+        <section></section>
+        <form>
+            <h1>Scale Swcure</h1>
+            <div>
+                <h2>sign up</h2>
+                <div>
+                    <label htmlFor="">Email</label>
+                    <input type="email" name="" id="" />
+                </div>
+                <input type="submit" value="Generate OTP" />
+                <div><hr /> or <hr /></div>
+                <div>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+                <small>Already have an account? <a href=""></a></small>
+            </div>
+        </form>
+    </main>
+  )
+}
+
+export default Signup
