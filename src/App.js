efd3f337d6ff973bff27ed39c3ec194b630bab2d
Sanjay Sanjay <sanjaysanjay@sanjay15.local>
2024-08-06 22:11:56 +0530

    created Main pg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Signup from './Signup'
+import Main from './Main'
 
 const App = () => {
   return (
     <div>
-      <Signup />
+      {/* <Signup /> */}
+      <Main />
     </div>
   )
 }
